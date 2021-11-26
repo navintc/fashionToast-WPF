b9c90a53bb0c656dba536cdba9c9483311a75111
navintc <navin.thamindu@gmail.com>
2021-11-26 21:22:32 +0530

    Add project files.

---
+++ Classes/SQLServerConnections.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//newly added
+using System.Data;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace fashionApp2.Classes
+{
+    class SQLServerConnections
+    {
+        public static string GetConnectionStrings()
+        {
+            string strConString = ConfigurationManager.ConnectionStrings["conString"].ToString();
+            return strConString;
+        }
+
+        public static string sql;
+        public static SqlConnection con = new SqlConnection();
+        public static SqlCommand cmd = new SqlCommand("", con);
+        public static SqlDataReader rd;
+        public static DataTable dt;
+        public static SqlDataAdapter da;
+
+
+        //Opening the SQL Connection
+        public static void openConnection()
+        {
+            try
+            {
+                if(con.State == ConnectionState.Closed)
+                {
+                    con.ConnectionString = GetConnectionStrings();
+                    con.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The system failed to establish a connection."
+                    + Environment.NewLine + "Description: " + ex.Message.ToString(),
+                    "C# WPF Connect to SQL Server", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        public static void closeConnection()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//new
+using System.Data;
+using System.Data.SqlClient;
+using fashionApp2.Classes;
+
+namespace fashionApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //"SQLServerConnections" is from the sqlserverconnections.cs
+            SQLServerConnections.openConnection();
+
+            SQLServerConnections.sql = "SELECT [itemID], [name], [color] from items";
+
+
+            SQLServerConnections.cmd.CommandType = CommandType.Text;
+            SQLServerConnections.cmd.CommandText = SQLServerConnections.sql;
+
+            SQLServerConnections.da = new SqlDataAdapter(SQLServerConnections.cmd);
+            SQLServerConnections.dt = new DataTable();
+            SQLServerConnections.da.Fill(SQLServerConnections.dt);
+
+
+            newDG.ItemsSource = SQLServerConnections.dt.DefaultView;
+            SQLServerConnections.closeConnection();
+
+
+        }
+    }
+}
